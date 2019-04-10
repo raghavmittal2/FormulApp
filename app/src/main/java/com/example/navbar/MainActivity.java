16a18bc6c2c381c5b4a01611a1b1ef9b301b573e
Raghav Mittal <raghavmittal2@gmail.com>
2019-04-10 02:15:09 +0100

    90% done

--- app/src/main/java/com/example/navbar/MainActivity.java
+++ app/src/main/java/com/example/navbar/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.navbar;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -11,22 +15,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-    Typeface robotobold;
-    Typeface robotoregular;
+    public Fragment fragment = null;
+    public FragmentManager fragmentManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        robotobold = Typeface.createFromAsset(getAssets(), "fonts/Roboto-Bold.ttf");
-        robotoregular = Typeface.createFromAsset(getAssets(), "fonts/Roboto-Regular.ttf");
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -37,10 +39,12 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
+
         if(savedInstanceState == null){
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                new fragmentMaths()).commit();
-        navigationView.setCheckedItem(R.id.nav_activity_maths_homepage);}
+                new fragmentHome()).commit();
+        navigationView.setCheckedItem(R.id.nav_activity_home);}
+
     }
 
     @Override
@@ -87,10 +91,12 @@ public class MainActivity extends AppCompatActivity
         } else if (id == R.id.nav_activity_physics_homepage) {
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
                     new fragmentPhysics()).commit();
-        }
-          else if (id == R.id.nav_activity_units_homepage) {
+        } else if (id == R.id.nav_activity_units_homepage) {
              getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
                         new units_page()).commit();
+        } else if (id == R.id.nav_activity_home) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new fragmentHome()).commit();
         } else if (id == R.id.nav_share) {
             Toast.makeText(this, "Share", Toast.LENGTH_SHORT).show();
         } else if (id == R.id.nav_send) {
